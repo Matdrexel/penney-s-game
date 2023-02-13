3c302a24aa801a8d6f605d6913f70ecace0cba8a
peterpeng <ppeng01@students.cs.ubc.ca>
2023-02-12 16:16:09 -0800

    StarBounds and star_bounds.txt

---
+++ StarBounds.py
@@ -0,0 +1,75 @@
+# Check bounds of A*A
+
+results = []
+
+p = 0.5
+
+
+def search(start, end):
+    i = start
+    while i <= end:
+        for m in range(2 ** i):
+            a = zeroed_bin_list(m, i)
+            print(star(a, a), a)
+            curr = [star(a, a), a]
+            entry = ', '.join(str(x) for x in curr)
+            results.append(entry)
+        i += 1
+    to_txt()
+
+
+def to_txt():
+    with open('star_bounds.txt', 'w') as f:
+        f.writelines('\n'.join(results))
+    print("done")
+
+
+def zeroed_bin_list(i, size):
+    result = []
+    r = bin_list(i)
+    for j in range(size - len(r)):
+        result.append(0)
+    result.extend(r)
+    return result
+
+
+def bin_list(x):
+    if x == 0:
+        return [0]
+    bit = []
+    while x:
+        bit.append(x % 2)
+        x >>= 1
+    return bit[::-1]
+
+
+def star(str1, str2):
+    result = 0
+    m = len(str1)
+    for i in range(m):
+        product = 1
+        for j in range(i, m):
+            product *= delta(j, j - i, str1, str2)
+        result += product
+    return result
+
+
+def delta(i, j, str1, str2):
+    if (0 <= i < len(str1)) & (0 <= j < len(str2)):
+        if str1[i] == str2[j]:
+            return inverse_p(str2[j])
+        else:
+            return 0
+    else:
+        return 0
+
+
+def inverse_p(i):
+    return 2
+    # if i == 0:
+    #     return 1 / p
+    # elif i == 1:
+    #     return 1 / (1 - p)
+
+
+search(1, 6)
